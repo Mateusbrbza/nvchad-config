27dc61bdbb45cc59f3d463afe30fe29325c84721
mateusbrbza <mateus.c.barboza@gmail.com>
2025-10-09 21:21:24 -0300

    feat: added avante.nvim

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,6 +3,7 @@ require "nvchad.mappings"
 -- Import modular mapping configurations
 local basic_mappings = require "mappings.basic"
 local ai_mappings = require "mappings.ai"
+local avante_mappings = require "mappings.avante"
 local folding_mappings = require "mappings.folding"
 local git_mappings = require "mappings.git"
 local lsp_mappings = require "mappings.lsp"
@@ -12,6 +13,7 @@ local ui_mappings = require "mappings.ui"
 -- Setup all mappings
 basic_mappings.setup()
 ai_mappings.setup()
+avante_mappings.setup()
 folding_mappings.setup()
 git_mappings.setup()
 lsp_mappings.setup()
