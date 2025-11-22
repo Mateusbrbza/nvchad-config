ebf965057fc62cf651974083666a5576731cd13a
mateusbrbza <mateus.c.barboza@gmail.com>
2025-11-22 18:56:33 -0300

    improvements on eslint.lua for current projects

--- lua/configs/lsp/eslint.lua
+++ lua/configs/lsp/eslint.lua
@@ -10,6 +10,27 @@ return {
     rulePaths = {},
     useESLintClass = false,
     experimental = { useFlatConfig = false },
-    codeActionOnSave = { enable = true, mode = "all" },
+    codeActionOnSave = { 
+      enable = true, 
+      mode = "all" 
+    },
+    workingDirectories = {
+      mode = "auto",
+    },
   },
+  root_dir = function(fname)
+    local lspconfig = require("lspconfig")
+    return lspconfig.util.root_pattern(
+      ".eslintrc",
+      ".eslintrc.js",
+      ".eslintrc.cjs",
+      ".eslintrc.yaml",
+      ".eslintrc.yml",
+      ".eslintrc.json",
+      "eslint.config.js",
+      "eslint.config.mjs",
+      "eslint.config.cjs",
+      "package.json"
+    )(fname) or lspconfig.util.find_git_ancestor(fname)
+  end,
 }
